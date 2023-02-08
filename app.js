2b3976b808b4e0ae46f61168c9d9b2d430a09eee
Mary <ngangamary078@gmail.com>
2023-02-08 14:31:02 +0300

    added insertions folders files and did some deletions

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.set('view engine', 'ejs')
+
+const posts = [
+    {title: 'Title 1', body: 'Body 1' },
+    {title: 'Title 2', body: 'Body 2' },
+    {title: 'Title 3', body: 'Body 3' },
+    {title: 'Title 4', body: 'Body 4' },
+]
+const user = {
+    firstName: 'Mary',
+    lastName: 'Wanjiku',
+}
+
+
+app.get('/', (req, res) => {
+    res.render('pages/index', {
+        user,
+        title: "Home Page"
+    })
+})
+app.get('/articles', (req, res) => {
+    res.render('pages/articles', {
+        articles: posts,
+        title: "Articles"
+    })
+})
+app.get('/about', (req, res) => {
+    res.render('pages/about', {
+        about: posts,
+        title: "About"
+    })
+})
+
+app.listen(port, () => {
+  console.log(`App listening at port ${port}`)
+})
